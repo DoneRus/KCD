3567de7e631e7577534bf62749bd95f9940d032f
Christiaan Stuut <6030461@mborijnland.nl>
2026-01-28 10:41:29 +0100

    begin aanmelden
    begin voor aanmelden

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="index.php" method="post">
+        <h1>Aanmelden</h1>
+        <label>Gebruikersnaam:</label>
+        <input type="text" name="gebruikersnaam">
+        <label>Wachtwoord:</label>
+        <input type="password" name="wachtwoord">
+        <input type="submit" value="Verzenden">
+    </form>
+</body>
+</html>
+
 <?php
 
 Echo "Hello World"
